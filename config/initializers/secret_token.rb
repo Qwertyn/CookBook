7e670e16cde28f7e50ee3066aea4ecf5882e9ccc
Aleksandr <salkovaleksandr@rambler.ru>
2011-03-17 14:55:48 +0000

    Создано новое приложение Кулинарная книга

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cookbook::Application.config.secret_token = '5f7f8685dbd0a13818ca73a4b94415ddf1d3df4c79db72b2fc3fc6f84486ac48ce67c7499900112470f7e871656723a25e0f805e66968d5c4d2d6f838c4fd079'
